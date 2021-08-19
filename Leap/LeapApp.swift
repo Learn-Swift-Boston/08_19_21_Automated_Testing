8e74e45bb1d47b1749ab5d902d12444b967f35d0
Matt Dias <matt.dias6@gmail.com>
2021-08-19 19:01:50 -0400

    Initial Commit

---
+++ Leap/LeapApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeapApp.swift
+//  Leap
+//
+//  Created by Matthew Dias on 8/19/21.
+//
+
+import SwiftUI
+
+@main
+struct LeapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
